b10376a959b61375f0cafcbf9e8b09af22650713
Emma Ramos <20001039@alumnos.utleon.edu.mx>
2024-11-06 18:49:08 -0600

    servicios terminados 06/11/2024

--- src/config.py
+++ src/config.py
@@ -1,11 +1,12 @@
-class DevolopmentConfig():
-    DEBUG = True
-    MSQL_HOST='127.0.0.1'
-    MSQL_USER='cardiel2'
-    MSQL_PASSWORD='root'
-    MSQL_DB='api_UTL'
-    
-    
-    config={
-        'devolopment': DevolopmentConfig
-    }
+class DevelopmentConfig:
+    DEBUG : True
+    MYSQL_HOST='127.0.0.1'
+    MYSQL_USER='root'
+    MYSQL_PASSWORD=''
+    MYSQL_DB='api_utl'
+ 
+config={
+ 
+    'development':DevelopmentConfig
+ 
+}
